4484829133bf3a4aba8d3e2b92ea4c4bf711be36
jetskeedog <jetskee96@yahoo.com>
2015-06-24 00:21:47 +0000

    Added devise/users

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/about'
-
-  get 'static_pages/privacy'
-
-  get 'static_pages/contact'
+ 
+ 
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :users
+  
+  get 'about',   to: 'static_pages#about'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
